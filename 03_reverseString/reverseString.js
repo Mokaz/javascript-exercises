51ff824914cadeb35888f811fd8873fb234445d2
Mokaz <martiniuss.wan@gmail.com>
2022-07-24 16:42:07 +0200

    a

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    array = string.split("");
+    result = (array.reverse()).join("");
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const result = [];
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            result.push(item);
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
